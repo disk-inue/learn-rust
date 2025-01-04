1999529d90793e80dfec5e03976d78468b45cbad
disk-inue <disk.inue.01@gmail.com>
2025-01-04 13:08:21 +0900

    feat : calculate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-/* fn main() {
-    println!("Hello, world!");
-} */
+use std::io::stdin;
 
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+enum FourArithmeticOperators {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-    let mut writter = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writter).unwrap();
+    let mut arg = String::new();
+    let mut args: Vec<String> = Vec::new();
+    let mut result = 0;
+
+    println!("start calculator");
+
+    println!("please number >");
+    stdin().read_line(&mut arg).expect("Failed to read line");
+    args.push(arg.parse().expect("Failed to parse"));
+
+    println!("please four arithmetic operators(+, -, *, /) >");
+    stdin().read_line(&mut arg).expect("Failed to read line");
+
+    let parsed_arg: String = arg.parse().expect("Failed to parse");
+
+    /* match parsed_arg.to_string() {
+        "+" => args.push(parsed_arg),
+        "-" => args.push(parsed_arg),
+        "*" => args.push(parsed_arg),
+        "/" => args.push(parsed_arg),
+    } */
+    args.push(arg.parse().expect("Failed to parse"));
+
+    println!("please number >");
+    stdin().read_line(&mut arg).expect("Failed to read line");
+    args.push(arg.parse().expect("Failed to parse"));
+
+    println!("please four arithmetic operators or enter >");
+    stdin().read_line(&mut arg).expect("Failed to read line");
+    args.push(arg.parse().expect("Failed to parse"));
+
+    println!("{}", arg);
 }
