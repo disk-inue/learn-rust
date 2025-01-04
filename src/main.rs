c7cdf30b6afbcb23bce9b401a3410b45ed1a0b81
disk-inue <disk.inue.01@gmail.com>
2025-01-04 14:34:40 +0900

    feat : calculate

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,59 @@
 use std::io::stdin;
 
-enum FourArithmeticOperators {
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
-
 fn main() {
-    let mut arg = String::new();
-    let mut args: Vec<String> = Vec::new();
+    let mut args: Vec<&str> = Vec::new();
     let mut result = 0;
 
     println!("start calculator");
 
     println!("please number >");
+    let mut arg = String::new();
     stdin().read_line(&mut arg).expect("Failed to read line");
-    args.push(arg.parse().expect("Failed to parse"));
+    args.push(arg.trim());
 
+    let mut arg = String::new();
     println!("please four arithmetic operators(+, -, *, /) >");
     stdin().read_line(&mut arg).expect("Failed to read line");
-
-    let parsed_arg: String = arg.parse().expect("Failed to parse");
-
-    /* match parsed_arg.to_string() {
+    let parsed_arg = arg.trim();
+    match parsed_arg {
         "+" => args.push(parsed_arg),
         "-" => args.push(parsed_arg),
         "*" => args.push(parsed_arg),
         "/" => args.push(parsed_arg),
-    } */
-    args.push(arg.parse().expect("Failed to parse"));
+        _ => {
+            println!("please four arithmetic operators(+, -, *, /)");
+            return;
+        }
+    }
 
+    let mut arg = String::new();
     println!("please number >");
     stdin().read_line(&mut arg).expect("Failed to read line");
-    args.push(arg.parse().expect("Failed to parse"));
+    args.push(arg.trim());
 
+    let mut arg = String::new();
     println!("please four arithmetic operators or enter >");
     stdin().read_line(&mut arg).expect("Failed to read line");
-    args.push(arg.parse().expect("Failed to parse"));
+    args.push(arg.trim());
 
-    println!("{}", arg);
+    for n in 0..args.len() {
+        if n == 0 {
+            result += args[n].parse::<i32>().unwrap();
+            continue;
+        }
+        if n % 2 == 0 {
+            let num = args[n].parse::<i32>().unwrap();
+            match args[n - 1] {
+                "+" => result += num,
+                "-" => result -= num,
+                "*" => result *= num,
+                "/" => result /= num,
+                _ => {
+                    println!("please four arithmetic operators(+, -, *, /)");
+                    return;
+                }
+            }
+        }
+    }
+    println!("{}", result)
 }
