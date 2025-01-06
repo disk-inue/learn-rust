4d1c38cf796f8b157c5b8bc611349c542caf2c74
disk-inue <disk.inue.01@gmail.com>
2025-01-06 13:11:04 +0900

    feat : 小数点対応 & 処理分割

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     println!("start calculator");
@@ -6,10 +6,8 @@ fn main() {
 
     loop {
         let mut args: Vec<&str> = Vec::new();
-        let mut result = 0;
 
-        println!("number >");
-        let input_left_number = input();
+        let input_left_number = input("number > ".to_string());
         match input_left_number.as_str() {
             "q" => {
                 println!("end calculator");
@@ -19,8 +17,7 @@ fn main() {
         }
         args.push(&input_left_number);
 
-        println!("+, -, *, / >");
-        let input_arithmetic = input();
+        let input_arithmetic = input("+, -, *, / > ".to_string());
         match input_arithmetic.as_str() {
             "+" => args.push(&input_arithmetic),
             "-" => args.push(&input_arithmetic),
@@ -31,8 +28,7 @@ fn main() {
             }
         }
 
-        println!("number >");
-        let input_right_number = input();
+        let input_right_number = input("number > ".to_string());
         match input_left_number.as_str() {
             "q" => {
                 println!("end calculator");
@@ -42,24 +38,7 @@ fn main() {
         }
         args.push(&input_right_number);
 
-        for n in 0..args.len() {
-            if n == 0 {
-                result += args[n].parse::<i32>().unwrap();
-                continue;
-            }
-            if n % 2 == 0 {
-                let num = args[n].parse::<i32>().unwrap();
-                match args[n - 1] {
-                    "+" => result += num,
-                    "-" => result -= num,
-                    "*" => result *= num,
-                    "/" => result /= num,
-                    _ => {
-                        break;
-                    }
-                }
-            }
-        }
+        let result: f32 = calculate(&args);
         println!(
             "calculate > {} {} {} = {}",
             args[0], args[1], args[2], result
@@ -67,8 +46,33 @@ fn main() {
     }
 }
 
-fn input() -> String {
+fn input(message: String) -> String {
+    print!("{}", &message);
+    stdout().flush().unwrap();
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read line");
     return input.trim().to_string();
+}
+
+fn calculate(args: &Vec<&str>) -> f32 {
+    let mut result: f32 = 0.0;
+    for n in 0..args.len() {
+        if n == 0 {
+            result += args[n].parse::<f32>().unwrap();
+            continue;
+        }
+        if n % 2 == 0 {
+            let num = args[n].parse::<f32>().unwrap();
+            match args[n - 1] {
+                "+" => result += num,
+                "-" => result -= num,
+                "*" => result *= num,
+                "/" => result /= num,
+                _ => {
+                    return 0.0;
+                }
+            }
+        }
+    }
+    return result;
 }
