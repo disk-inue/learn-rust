5447868a40e70b85f91d73e9f9424c452f79e674
disk-inue <disk.inue.01@gmail.com>
2025-01-07 22:15:50 +0900

    feat : データ型変更

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ fn main() {
     loop {
         let mut args: Vec<&str> = Vec::new();
 
-        let input_left_number = input("number > ".to_string());
-        match input_left_number.as_str() {
-            "q" => {
-                println!("end calculator");
-                break;
-            }
-            _ => {}
+        let input_left_number = input("number > ");
+        if input_left_number == "q" {
+            println!("end calculator");
+            break;
         }
         args.push(&input_left_number);
 
-        let input_arithmetic = input("+, -, *, / > ".to_string());
+        let input_arithmetic = input("+, -, *, / > ");
         match input_arithmetic.as_str() {
             "+" => args.push(&input_arithmetic),
             "-" => args.push(&input_arithmetic),
@@ -28,41 +25,35 @@ fn main() {
             }
         }
 
-        let input_right_number = input("number > ".to_string());
-        match input_left_number.as_str() {
-            "q" => {
-                println!("end calculator");
-                break;
-            }
-            _ => {}
+        let input_right_number = input("number > ");
+        if input_right_number == "q" {
+            println!("end calculator");
+            break;
         }
         args.push(&input_right_number);
 
-        let result: f32 = calculate(&args);
-        println!(
-            "calculate > {} {} {} = {}",
-            args[0], args[1], args[2], result
-        )
+        let result = calculate(&args);
+        println!("calculate > {} = {}", args.join(" "), result)
     }
 }
 
-fn input(message: String) -> String {
+fn input(message: &str) -> String {
     print!("{}", &message);
     stdout().flush().unwrap();
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read line");
-    return input.trim().to_string();
+    input.trim().to_string()
 }
 
-fn calculate(args: &Vec<&str>) -> f32 {
-    let mut result: f32 = 0.0;
+fn calculate(args: &Vec<&str>) -> f64 {
+    let mut result: f64 = 0.0;
     for n in 0..args.len() {
         if n == 0 {
-            result += args[n].parse::<f32>().unwrap();
+            result += args[n].parse::<f64>().unwrap();
             continue;
         }
         if n % 2 == 0 {
-            let num = args[n].parse::<f32>().unwrap();
+            let num = args[n].parse::<f64>().unwrap();
             match args[n - 1] {
                 "+" => result += num,
                 "-" => result -= num,
@@ -74,5 +65,5 @@ fn calculate(args: &Vec<&str>) -> f32 {
             }
         }
     }
-    return result;
+    result
 }
