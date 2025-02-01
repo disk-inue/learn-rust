785ff57074e3aecd7cf4f38cb58cb8fd441282b4
disk-inue <disk.inue.01@gmail.com>
2025-02-01 11:26:11 +0900

    feat : dice app

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod calculator;
 mod common;
+mod dice;
 mod todo;
 
 fn main() {
     println!("select module");
     println!("1. calulator");
     println!("2. todo");
+    println!("3. dice");
     println!("q is end module");
 
     let select_number: u8 = match common::input("module > ").parse() {
@@ -18,6 +20,7 @@ fn main() {
     match select_number {
         1 => calculator::exec(),
         2 => todo::exec(),
+        3 => dice::exec(),
         _ => {
             println!("end module");
             return;
