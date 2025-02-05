2df0d56f830bdfda8600a6b6256c0052169c0bcd
disk-inue <disk.inue.01@gmail.com>
2025-02-05 21:06:38 +0900

    feat : dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-mod calculator;
+/* mod calculator;
 mod common;
 mod dice;
-mod todo;
+mod todo; */
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
-    println!("select module");
+    /* println!("select module");
     println!("1. calulator");
     println!("2. todo");
     println!("3. dice");
     println!("q is end module");
-
     let select_number: u8 = match common::input("module > ").parse() {
         Ok(num) => num,
         Err(_) => {
@@ -23,7 +27,34 @@ fn main() {
         3 => dice::exec(),
         _ => {
             println!("end module");
-            return;
+            return; */
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Hero {}
+
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {
+            id: "hero",
+            img { src: HEADER_SVG, id: "header" }
+            div { id: "links",
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            }
         }
     }
 }
