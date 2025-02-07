4dbbcbf047f2edf54145567d0c02c1d6962362b7
disk-inue <disk.inue.01@gmail.com>
2025-02-07 10:26:30 +0900

    feat : css 適用

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use dioxus::prelude::*;
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
+static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     /* println!("select module");
@@ -34,17 +35,17 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+      document::Stylesheet {href: CSS}
       div {id: "title",
-            h1 {"HotDog!"}
+        h1 {"HotDog!"}
       }
-        div { id: "dogview",
-          img { src:"https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
-        }
+      div { id: "dogview",
+        img { src:"https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+      }
       div { id: "buttons",
         button {id: "skip", "skip"}
         button {id: "save", "save!"}
-
-    }
+      }
     }
 }
 
